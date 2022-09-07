392435250e3b42deda464642c10d7bfc199809e0
Demian Tavolaro <DTavolaro@gruposolvens.com.ar>
2022-09-07 18:14:03 -0300

    DOM: Atributos

--- js/dom.js
+++ js/dom.js
@@ -14,3 +14,16 @@ console.log(document.querySelector("#articulo"));
 //Para seleccionar todos los elementos podemos usar:
 console.log(document.querySelectorAll(".opciones"));
 console.log(document.querySelectorAll(".opciones")[2]); // Seleccionamos el tercer elemento de clase "opciones"
+
+// Interactuar con atributos
+
+console.log(document.documentElement.lang); //accedemos al atributo lang del html
+
+console.log(document.querySelector(".enlace").href);
+
+document.querySelector(".enlace").href= "https://www.facebook.com"; // De esta manera modificamos la direccion href
+
+const $enlaceDOM = document.querySelector(".enlace"); //Guardamos el enlace en una constante
+
+$enlaceDOM.setAttribute("target", "_blank"); // De esta manera podemos añadir un nuevo atributo
+
