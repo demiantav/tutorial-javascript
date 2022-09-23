67ffe886b9d2eeec45d24ecae6c06dffe1221eff
Demian Tavolaro <DTavolaro@gruposolvens.com.ar>
2022-09-23 16:32:15 -0300

    DOM: Estilos y variables CSS

--- js/dom.js
+++ js/dom.js
@@ -1,4 +1,4 @@
-console.log("que haces");
+/*console.log("que haces");
 console.log(document.body);
 console.log(document.head);
 console.log(document.scripts);
@@ -23,7 +23,28 @@ console.log(document.querySelector(".enlace").href);
 
 document.querySelector(".enlace").href= "https://www.facebook.com"; // De esta manera modificamos la direccion href
 
-const $enlaceDOM = document.querySelector(".enlace"); //Guardamos el enlace en una constante
+const $enlaceDOM = document.querySelector(".enlace");
+console.log($enlaceDOM.getAttribute("href"));
+console.log($enlaceDOM); //Guardamos el enlace en una constante
 
 $enlaceDOM.setAttribute("target", "_blank"); // De esta manera podemos añadir un nuevo atributo
+*/
+
+const $bodyDOM= (document.querySelector("body"));
+const bodyStyles = window.getComputedStyle($bodyDOM); // Guardamos el listado de estilos de la etiqueta body y luego lo imprimimos, se genera un objeto llamado "CSSStyleDeclaration" (Unicamente de lectura):
+console.log(bodyStyles);
+console.log(bodyStyles.backgroundColor); // Imprimimos el valor de la propiedad background-color
+
+//Otra forma mas facil es la siguiente:
+console.log($bodyDOM.style); // Imprimimos el mismo objeto CSSStyleDeclaration
+$bodyDOM.style.backgroundColor="hsl(194, 25%, 81%)"; //Modificamos el valor de la propiedad elegida
+
+$bodyDOM.style.setProperty("color", "black"); // Otra manera de modificar propiedades
+
+let varFont = getComputedStyle(document.documentElement).getPropertyValue("--font-title"); // De esta manera accedemos a una variable css
+
+$bodyDOM.style.fontSize= varFont; // Y podemos asignarla a una propiedad que queramos
+
+
+
 
