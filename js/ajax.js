81e6705d9ea8f3686f13f576eb7059b650862e11
Demian Tavolaro <demiantavolaro6@gmail.com>
2023-06-15 19:08:03 -0300

    Ajax: cambio de mail git

--- js/ajax.js
+++ js/ajax.js
@@ -2,7 +2,7 @@ const xhr = new XMLHttpRequest();
 
 xhr.addEventListener("readystatechange", e => {
    
-    if(xhr.readyState!==4) return; // Mediante esta validacion nos aseguramos que el codigo debajo solo se ejecute cuando el estado se complete (4)
+    if(xhr.readyState!==4) return; // Mediante esta validacion nos aseguramos que el codigo debajo solo se ejecute cuando el estado se complete (4) -------
     if(xhr.status >= 200 && xhr.status < 300){
 
         console.log("ok");
@@ -10,7 +10,7 @@ xhr.addEventListener("readystatechange", e => {
         console.log(JSON.parse(data));
     } else {
 
-        console.log("not");
+        console.log("no");
         console.log(xhr.responseText);
     }
 
