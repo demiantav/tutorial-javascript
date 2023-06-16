4f66829c8358036e51a7573daec9ac76b9bd8592
Demian Tavolaro <demiantavolaro6@gmail.com>
2023-06-16 17:00:30 -0300

    Ajax: prueba de commit

--- js/ajax.js
+++ js/ajax.js
@@ -1,22 +1,27 @@
 const xhr = new XMLHttpRequest();
 
 xhr.addEventListener("readystatechange", e => {
+
+    let data;
    
-    if(xhr.readyState!==4) return; // Mediante esta validacion nos aseguramos que el codigo debajo solo se ejecute cuando el estado se complete (4) -------
+    if(xhr.readyState!==4) return; // Mediante esta validacion nos aseguramos que el codigo debajo solo se ejecute cuando el estado se complete (4)
     if(xhr.status >= 200 && xhr.status < 300){
 
         console.log("ok");
-        let data = xhr.responseText;
-        console.log(JSON.parse(data));
-    } else {
+        data = xhr.responseText;
+        const $arraydata = JSON.parse(data);
+        console.log($arraydata);
 
+    } else {
+        
         console.log("no");
         console.log(xhr.responseText);
     }
 
-    console.log(xhr);
+    
 })
 
 xhr.open("GET","https://jsonplaceholder.typicode.com/users");
 
 xhr.send();
+
